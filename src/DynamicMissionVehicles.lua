4e28c077cfd9ef0d4cf60e26e55c9e6c7493bd3e
tn47 <tn4799@gmail.com>
2024-01-16 21:26:25 +0100

    fixed typo in method name

--- src/DynamicMissionVehicles.lua
+++ src/DynamicMissionVehicles.lua
@@ -199,7 +199,7 @@ function DynamicMissionVehicles:loadVehicles(xmlFilename, baseDirectory)
 				end
 
 				if (modName == nil or modName == "") and not filename:startsWith("$") then
-					Logging.Warning("Cannot load missionVehicle that is neither part of the baseGame nor part of a mod.")
+					Logging.warning("Cannot load missionVehicle that is neither part of the baseGame nor part of a mod.")
 					k = k + 1
 					goto continue
 				end
